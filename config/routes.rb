dd3030725d69f228da6bd1aaf9e4bb4727ccbb32
skj <skj.jo0c@gmail.com>
2017-11-03 17:52:29 +0545

    adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :advertisements
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
   	namespace 'v1' do
