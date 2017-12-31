3ce24a404000b88ecaa9cf2d89d96f068a2468db
skj <skj.jo0c@gmail.com>
2017-12-31 14:14:27 +0545

    delete

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,6 +1,6 @@
 class Advertisement < ApplicationRecord
 	validates :name, :price, :description, presence: true
-	has_many :adphotos
+	has_many :adphotos, :dependent => :destroy
 	attr_accessor :picture_data
 	belongs_to :user
 	has_and_belongs_to_many :categories
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	validates_uniqueness_of :email
 	validates :firstname, :lastname, :email, :mobile, presence: true
 	validates_presence_of :password_confirmation
-	has_many :advertisements
+	has_many :advertisements, :dependent => :destroy
 	mount_uploader :picture, ProfilePictureUploader
 	has_secure_password
 end
